36944137acbe03cd3539ab32adf80f7a7b214541
Elijah Smith <elijahs2106@gmail.com>
2018-07-02 15:29:28 -0400

    Extract article links into seperate component

--- src/Article.js
+++ src/Article.js
@@ -1,29 +1,16 @@
 import React from 'react';
 
-import ArticleText from './ArticleText.js';
+import ArticleText from './ArticleText';
+import ArticleLinks from './ArticleLinks';
+import ArticleAuthor from './ArticleAuthor';
 
 const Article = () => {
     return (
         <div className="large-8 medium-12 columns article">
             <h2 className="article-title">Gold Madness - Fact or Fiction?</h2>
-            <div className="avatar">
-                <img src="http://www.zbrushcentral.com/attachment.php?attachmentid=376082" alt="author" />
-                <div className="author-info">
-                    <p className="author-name">By Domri, son of Flug</p>
-                    <p className="date">on 28 April, 3018 of the Third Age</p>
-                </div>
-            </div>
+            <ArticleAuthor />
             <ArticleText />
-            <div className="article-links">
-                <a className="article-link" href="#">
-                    <i className="fa fa-comments-o"></i>
-                    <span className="article-link-text">Comments</span>
-                </a>
-                <a className="article-link" href="#">
-                    <i className="fa fa-share"></i>
-                    <span className="article-link-text">Share Post</span>
-                </a>
-            </div>
+            <ArticleLinks />
         </div>
     );
 };
