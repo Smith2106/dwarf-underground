5a069f3f05e4f73ffe66c72f5594b9bf9b70ed62
Elijah Smith <elijahs2106@gmail.com>
2018-07-02 22:22:40 -0400

    Add arrows to allow voting on comments

--- src/Comment.js
+++ src/Comment.js
@@ -3,8 +3,14 @@ import React from 'react';
 const Comment = props => {
     return (
         <li className="comment">
-            <span className="comment-username"><i className="fa fa-user"></i>  {props.username}</span>
-            <span className="comment-content"><i className="fa fa-commenting"></i>  {props.content}</span>
+            <span className="comment-username">
+                <i className="fa fa-user"></i>  {props.username}
+                <span className="vote upvote"><i className="fa fa-arrow-up"></i></span>
+            </span>
+            <span className="comment-content">
+                <i className="fa fa-commenting"></i>  {props.content}
+                <span className="vote downvote"><i className="fa fa-arrow-down"></i></span>
+            </span>
         </li>
     );
 };
